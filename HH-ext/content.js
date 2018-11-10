8cee811f94e429518a4f10108c64136862709363
xaky20 <xaky20@gmail.com>
2018-11-10 10:13:57 -0500

    Add switch to disable & form to changes variables

--- HH-ext/content.js
+++ HH-ext/content.js
@@ -64,21 +64,126 @@ function setXKFight(xkworld, xkfight, xkmin) {
 	setXKVariable('XKMINENERGY', xkmin);
 }
 
-getXKVariable('XKWORLD', function() {
-	if (!xkmap['XKWORLD']) {
-		setXKVariable('XKWORLD', 10);
-	}
-});
-getXKVariable('XKFIGHTENERGY', function() {
-	if (!xkmap['XKFIGHTENERGY']) {
-		setXKVariable('XKFIGHTENERGY', 18);
-	}
-});
-getXKVariable('XKMINENERGY', function() {
-	if (!xkmap['XKMINENERGY']) {
-		setXKVariable('XKMINENERGY', 15);
-	}
-});
+function getXKFightVariables() {
+	getXKVariable('XKWORLD', function() {
+		if (!xkmap['XKWORLD']) {
+			setXKVariable('XKWORLD', 10);
+		}
+	});
+	getXKVariable('XKFIGHTENERGY', function() {
+		if (!xkmap['XKFIGHTENERGY']) {
+			setXKVariable('XKFIGHTENERGY', 18);
+		}
+	});
+	getXKVariable('XKMINENERGY', function() {
+		if (!xkmap['XKMINENERGY']) {
+			setXKVariable('XKMINENERGY', 15);
+		}
+	});
+}
+
+function getXKSwitch(callback) {
+	getXKVariable('XKSWITCH', function() {
+		if (typeof xkmap['XKSWITCH'] === 'undefined') {
+			setXKVariable('XKSWITCH', false);
+		}
+		if (callback) {
+			callback();
+		}
+	});
+}
+
+function createXKVariableForm() {
+	var xkdiv = document.createElement('div');
+	xkdiv.id = 'xkdiv';
+	xkdiv.style.position = 'absolute';
+	xkdiv.style.bottom = '40px';
+	xkdiv.style.right = '40px';
+	xkdiv.style.zIndex = 400;
+
+	var xkspan = document.createElement('span');
+	xkspan.innerHTML = 'World: ';
+	xkspan.style.color = '#000';
+	xkdiv.appendChild(xkspan);
+
+	var xkinput = document.createElement('input');
+	xkinput.id = 'xkworld';
+	xkinput.type = 'number';
+	xkinput.style.marginRight = '5px';
+	xkinput.min = 0;
+	xkinput.max = 10;
+	xkdiv.appendChild(xkinput);
+
+	var xkspan = document.createElement('span');
+	xkspan.innerHTML = 'Fight Energy: ';
+	xkspan.style.color = '#000';
+	xkdiv.appendChild(xkspan);
+
+	var xkinput = document.createElement('input');
+	xkinput.id = 'xkfightenergy';
+	xkinput.type = 'number';
+	xkinput.style.marginRight = '5px';
+	xkinput.min = 0;
+	xkinput.max = 10;
+	xkdiv.appendChild(xkinput);
+
+	var xkspan = document.createElement('span');
+	xkspan.innerHTML = 'Min Energy: ';
+	xkspan.style.color = '#000';
+	xkdiv.appendChild(xkspan);
+
+	var xkinput = document.createElement('input');
+	xkinput.id = 'xkminenergy';
+	xkinput.type = 'number';
+	xkinput.style.marginRight = '5px';
+	xkinput.min = 0;
+	xkinput.max = 10;
+	xkdiv.appendChild(xkinput);
+
+	var xkinput = document.createElement('input');
+	xkinput.id = 'xkapply';
+	xkinput.type = 'button';
+	xkinput.classList.add('green_text_button');
+	xkinput.style.marginRight = '5px';
+	xkinput.value = 'Apply';
+	xkinput.addEventListener("click", function() {
+		var xkWInput = document.getElementById('xkworld');
+		var xkFEInput = document.getElementById('xkfightenergy');
+		var xkMEInput = document.getElementById('xkminenergy');
+		var xkW = xkWInput.value - 0;
+		var xkFE = xkFEInput.value - 0;
+		var xkME = xkMEInput.value - 0;
+		if (xkW) {
+			setXKVariable('XKWORLD', xkW);
+			xkWInput.value = '';
+		}
+		if (xkFE) {
+			setXKVariable('XKFIGHTENERGY', xkFE);
+			xkFEInput.value = '';
+		}
+		if (xkME) {
+			setXKVariable('XKMINENERGY', xkME);
+			xkMEInput.value = '';
+		}
+	});
+	xkdiv.appendChild(xkinput);
+
+	var xkinput = document.createElement('input');
+	xkinput.id = 'xkswitch';
+	xkinput.type = 'button';
+	xkinput.classList.add('blue_text_button');
+	xkinput.value = 'Switch';
+	xkinput.addEventListener("click", function() {
+		setXKVariable('XKSWITCH', !xkmap['XKSWITCH'], function() {
+			if (window.location.href.indexOf('/home.html') === -1) {
+				window.location.reload();
+			}
+		});
+	});
+	xkdiv.appendChild(xkinput);
+
+	document.body.appendChild(xkdiv);
+}
 
 var login = document.querySelectorAll('a[rel="phoenix_member_login"]');
 
@@ -113,12 +218,16 @@ if (login.length !== 0) {
 			}, myRandom('login', 500, 3000));
 		});
 	});
-}
+
 
 /*** HOME ***/
-if (login.length === 0 && window.location.href.indexOf('/home.html') !== -1) {
+} else if (window.location.href.indexOf('/home.html') !== -1) {
 	console.log('HOME');
 	
+	getXKSwitch();
+	getXKFightVariables();
+	
+	createXKVariableForm();
 	
 	var checkCollect = function() {
 		var collect = document.getElementById('collect_all');
@@ -165,239 +274,270 @@ if (login.length === 0 && window.location.href.indexOf('/home.html') !== -1) {
 	var check_interval = function() {
 		setTimeout(function() {
 			checkCollect();
-			checkArena();
-			checkActivity();
-			checkEnergy();
-			checkFreeGame();
-			console.log('Check done');
+			if (!xkmap['XKSWITCH']) {
+				checkArena();
+				checkActivity();
+				checkEnergy();
+				checkFreeGame();
+				console.log('Check done');
+			} else {
+				console.log('Money check done');
+			}
 			check_interval();
 		}, myRandom('next check', 45000, 90000));
 	}
 
 	setTimeout(function() {
 		checkCollect();
-		checkArena();
-		checkActivity();
-		checkEnergy();
-		checkFreeGame();
+		if (!xkmap['XKSWITCH']) {
+			checkArena();
+			checkActivity();
+			checkEnergy();
+			checkFreeGame();
+		}
 		check_interval();
-	}, myRandom('first check', 500, 3000));
+	}, myRandom('first check', 1500, 3000));
 	
+} else {
+	createXKVariableForm();	
+	getXKSwitch(function() {
+		
+		if (xkmap['XKSWITCH']) {
+			console.log('HH-ext disabled');
+			setTimeout(function() {
+				window.location = '/home.html';
+			}, myRandom('home', 300000, 600000));
+		} else {
+			doXKThings();
+		}
+	});
+}
+
+function doXKThings() {
+		
 /*** ARENA ***/
-} else if (window.location.href.indexOf('/arena.html') !== -1) {
-	console.log('ARENA');
-	
-	var blueButtons = document.querySelectorAll('button.blue_text_button');
-	if (blueButtons[1].id === 'cancel_team') {
+	if (window.location.href.indexOf('/arena.html') !== -1) {
+		console.log('ARENA');
+		
+		var blueButtons = document.querySelectorAll('button.blue_text_button');
+		if (blueButtons[1].id === 'cancel_team') {
+			setTimeout(function() {
+				//window.location = '/intro.php?phoenix_member=logout'; infinite logout
+				window.location = '/tower-of-fame.html';
+			}, myRandom('towerOfFame', 1000, 3000));
+		} else {
+			setTimeout(function() {
+				//blueButtons[1].click();
+				xkevent(blueButtons[1], 'click');
+			}, myRandom('arena battle', 1000, 3000));
+		}
+		
 		setTimeout(function() {
-			//window.location = '/intro.php?phoenix_member=logout'; infinite logout
-			window.location = '/tower-of-fame.html';
-		}, myRandom('towerOfFame', 1000, 3000));
-	} else {
-		setTimeout(function() {
-			//blueButtons[1].click();
-			xkevent(blueButtons[1], 'click');
-		}, myRandom('arena battle', 1000, 3000));
-	}
-	
-	setTimeout(function() {
-		window.location = '/arena.html';
-	}, myRandom('arena', 6000, 12000));
-	
+			window.location = '/arena.html';
+		}, myRandom('arena', 6000, 12000));
+		
 /*** ARENA BATTLE ***/
-} else if (window.location.href.indexOf('/battle.html?id_arena') !== -1) {
-	console.log('ARENA BATTLE');
-	
-	setTimeout(function() {
-		//document.querySelectorAll('button[rel="launch"]')[0].click();
-		xkevent(document.querySelector('button[rel="launch"]'), 'click');
-	}, myRandom('fight', 1000, 4500));
-	
-	setTimeout(function() {
-		window.location = '/arena.html';
-	}, myRandom('arena', 4500, 9000));
-	
+	} else if (window.location.href.indexOf('/battle.html?id_arena') !== -1) {
+		console.log('ARENA BATTLE');
+		
+		setTimeout(function() {
+			//document.querySelectorAll('button[rel="launch"]')[0].click();
+			xkevent(document.querySelector('button[rel="launch"]'), 'click');
+		}, myRandom('fight', 1000, 4500));
+		
+		setTimeout(function() {
+			window.location = '/arena.html';
+		}, myRandom('arena', 4500, 9000));
+		
 /*** ACTIVITY ***/
-} else if (window.location.href.indexOf('/activities.html') !== -1) {
-	console.log('ACTIVITY');
-	
-	var blueButtons = document.querySelectorAll('button.blue_text_button');
-	var purpleButtons = document.querySelectorAll('button.purple_text_button');
-	
-	var claim = function() {
+	} else if (window.location.href.indexOf('/activities.html') !== -1) {
+		console.log('ACTIVITY');
+		
+		var blueButtons = document.querySelectorAll('button.blue_text_button');
 		var purpleButtons = document.querySelectorAll('button.purple_text_button');
-		for(var i = 0; i < purpleButtons.length; i++){
-			if (purpleButtons[i].style.display !== 'none') {
-				//purpleButtons[i].click();
-				xkevent(purpleButtons[i], 'click');
+		
+		var claim = function() {
+			var purpleButtons = document.querySelectorAll('button.purple_text_button');
+			for(var i = 0; i < purpleButtons.length; i++){
+				if (purpleButtons[i].style.display !== 'none') {
+					//purpleButtons[i].click();
+					xkevent(purpleButtons[i], 'click');
+					setTimeout(function() {
+						window.location = '/activities.html';
+					}, myRandom('activities', 1500, 4500));
+				}
+			}
+			
+			setTimeout(function() {
+				window.location = '/home.html';
+			}, myRandom('home', 4500, 10500));
+		}
+		
+		if (blueButtons[0].innerText.indexOf('Continue') !== -1) {
+			if (document.querySelector('.after_gift').style.display !== 'block') {
+				setTimeout(function() {
+					//document.querySelector('.end_gift button').click();
+					xkevent(document.querySelector('.end_gift button'), 'click');
+				}, myRandom('end gift', 1000, 3000));
 				setTimeout(function() {
 					window.location = '/activities.html';
-				}, myRandom('activities', 1500, 4500));
+				}, myRandom('activities', 3000, 5000));
+			} else {
+				claim();
+			}
+		} else {
+			if (blueButtons[0].style.display === 'none') {
+				claim();
+			} else {
+				setTimeout(function() {
+					//blueButtons[0].click();
+					xkevent(blueButtons[0], 'click');
+				}, myRandom('mission start', 1000, 3000));
+				setTimeout(function() {
+					window.location = '/intro.php?phoenix_member=logout';
+					//window.location = '/home.html';
+				}, myRandom('logout', 3000, 6000));
+			}
+		}
+		
+/*** TROLL BATTLE ***/
+	} else if (window.location.href.indexOf('/battle.html?id_troll') !== -1) {
+		var troll = window.location.href.substring(window.location.href.lastIndexOf('=') + 1) - 0;
+		console.log('TROLL BATTLE ' + troll);
+		
+		getXKFightVariables();
+		
+		setTimeout(function() {
+			if (xkmap['XKWORLD'] == (troll + 1) && document.querySelector('.energy_counter[type="energy_fight"] span[energy]').innerText > xkmap['XKMINENERGY']) {
+				setTimeout(function() {
+					//document.querySelectorAll('button[rel="launch"]')[0].click();
+					xkevent(document.querySelector('button[rel="launch"]'), 'click');
+				}, myRandom('fight', 1000, 3000));
+				
+				setTimeout(function() {
+					window.location = '/battle.html?id_troll='+(xkmap['XKWORLD']-1);
+				}, myRandom('reload', 3000, 6000));
+			} else {
+				setTimeout(function() {
+					window.location = '/home.html';
+				}, myRandom('home', 6000, 9000));
+			}
+		}, 1500);
+		
+/*** WORLD ***/
+	} else if (window.location.href.indexOf('/world/') !== -1) {
+		console.log('WORLD ' + window.location.href.substring(window.location.href.lastIndexOf('/') + 1));
+		
+		getXKFightVariables();
+		
+		setTimeout(function() {
+			if (document.querySelector('.energy_counter[type="energy_fight"] span[energy]').innerText > xkmap['XKFIGHTENERGY']) {
+				setTimeout(function() {
+					window.location = '/battle.html?id_troll='+(xkmap['XKWORLD']-1);
+				}, myRandom('troll', 1000, 3000));
+			} else {
+				setTimeout(function() {
+					window.location = '/home.html';
+				}, myRandom('home', 3000, 7500));
+			}
+		}, 1500);
+		
+/*** FREE PACHINKO ***/
+	} else if (window.location.href.indexOf('/pachinko.html') !== -1) {
+		console.log('FREE PACHINKO');
+		
+		if (document.querySelector('button.blue_text_button[free="1"]')) {
+			setTimeout(function() {
+				//document.querySelector('button.blue_text_button[free="1"]').click();
+				xkevent(document.querySelector('button.blue_text_button[free="1"]'), 'click');
+			}, myRandom('free', 5000, 8000));
+		}
+		setTimeout(function() {
+			window.location = '/home.html';
+		}, myRandom('home', 8000, 12000));
+		
+/*** TOWER OF FAME ***/
+	} else if (window.location.href.indexOf('/tower-of-fame.html') !== -1) {
+		console.log('TOWER OF FAME');
+		
+		var xaky_table = document.querySelectorAll('table')[5];
+		if (xaky_table) {
+			//check battle point
+			var battlePoint = (document.querySelectorAll('span[energy]')[2].innerText-0) || 0;
+			console.log('Challenge point: ' + battlePoint);
+			var canBattle = false;
+			var challenger, challengerFight;
+			var i = xaky_table.rows.length;
+			if (battlePoint > 0) {
+				//choose challenger
+				while (!canBattle) {
+					i--;
+					console.log(i);
+					if (i < 0) {
+						canBattle = true;
+					} else {
+						challenger = xaky_table.rows[i];
+						challengerFight = challenger.cells[3].innerText[0] - 0;
+						if (!challengerFight.isNaN && challengerFight < 3) {
+							canBattle = true;
+							//xaky_table.rows[i].click();
+							xkevent(xaky_table.rows[i], 'click');
+							setTimeout(function() {
+								//document.querySelector('.blue_button_L').click();
+								xkevent(document.querySelector('.blue_button_L'), 'click');
+							}, myRandom('challengeTower', 500, 1000));
+						}
+					}
+				}
+			}
+		} else {
+			var purpleButton = document.querySelector('.purple_button_L');
+			if (purpleButton.display !== 'none') {
+				xkevent(purpleButton, 'click');
 			}
 		}
 		
 		setTimeout(function() {
 			window.location = '/home.html';
-		}, myRandom('home', 4500, 10500));
-	}
-	
-	if (blueButtons[0].innerText.indexOf('Continue') !== -1) {
-		if (document.querySelector('.after_gift').style.display !== 'block') {
-			setTimeout(function() {
-				//document.querySelector('.end_gift button').click();
-				xkevent(document.querySelector('.end_gift button'), 'click');
-			}, myRandom('end gift', 1000, 3000));
-			setTimeout(function() {
-				window.location = '/activities.html';
-			}, myRandom('activities', 3000, 5000));
-		} else {
-			claim();
-		}
-	} else {
-		if (blueButtons[0].style.display === 'none') {
-			claim();
-		} else {
-			setTimeout(function() {
-				//blueButtons[0].click();
-				xkevent(blueButtons[0], 'click');
-			}, myRandom('mission start', 1000, 3000));
-			setTimeout(function() {
-				window.location = '/intro.php?phoenix_member=logout';
-				//window.location = '/home.html';
-			}, myRandom('logout', 3000, 6000));
-		}
-	}
-	
-/*** TROLL BATTLE ***/
-} else if (window.location.href.indexOf('/battle.html?id_troll') !== -1) {
-	console.log('TROLL BATTLE');
-	
-	setTimeout(function() {
-		if (xkmap['XKWORLD'] && document.querySelector('.energy_counter[type="energy_fight"] span[energy]').innerText > xkmap['XKMINENERGY']) {
-			setTimeout(function() {
-				//document.querySelectorAll('button[rel="launch"]')[0].click();
-				xkevent(document.querySelector('button[rel="launch"]'), 'click');
-			}, myRandom('fight', 1000, 3000));
-			
-			setTimeout(function() {
-				window.location = '/battle.html?id_troll='+(xkmap['XKWORLD']-1);
-			}, myRandom('world', 3000, 6000));
-		} else {
-			setTimeout(function() {
-				window.location = '/home.html';
-			}, myRandom('home', 6000, 9000));
-		}
-	}, 1500);
-	
-/*** WORLD ***/
-} else if (xkmap['XKWORLD'] && window.location.href.indexOf('/world/'+xkmap['XKWORLD']) !== -1) {
-	console.log('WORLD ' + xkmap['XKWORLD']);
-	
-	setTimeout(function() {
-		if (document.querySelector('.energy_counter[type="energy_fight"] span[energy]').innerText > xkmap['XKFIGHTENERGY']) {
-			setTimeout(function() {
-				window.location = '/battle.html?id_troll='+(xkmap['XKWORLD']-1);
-			}, myRandom('troll', 1000, 3000));
-		} else {
-			setTimeout(function() {
-				window.location = '/home.html';
-			}, myRandom('home', 3000, 7500));
-		}
-	}, 1500);
-	
-/*** FREE PACHINKO ***/
-} else if (window.location.href.indexOf('/pachinko.html') !== -1) {
-	console.log('FREE PACHINKO');
-	
-	if (document.querySelector('button.blue_text_button[free="1"]')) {
-		setTimeout(function() {
-			//document.querySelector('button.blue_text_button[free="1"]').click();
-			xkevent(document.querySelector('button.blue_text_button[free="1"]'), 'click');
-		}, myRandom('free', 5000, 8000));
-	}
-	setTimeout(function() {
-		window.location = '/home.html';
-	}, myRandom('home', 8000, 12000));
-	
-/*** INTRO ***/
-} else if (window.location.href.indexOf('/intro') !== -1) {
-	console.log('intro bugg ?');
-	
-	setTimeout(function() {
-		window.location = '/home.html';
-	}, myRandom('home', 6000, 9000));
-	
-/*** TOWER OF FAME ***/
-} else if (window.location.href.indexOf('/tower-of-fame.html') !== -1) {
-	console.log('TOWER OF FAME');
-	
-	var xaky_table = document.querySelectorAll('table')[5];
-	if (xaky_table) {
-		//check battle point
-		var battlePoint = (document.querySelectorAll('span[energy]')[2].innerText-0) || 0;
-		console.log('Challenge point: ' + battlePoint);
-		var canBattle = false;
-		var challenger, challengerFight;
-		var i = xaky_table.rows.length;
-		if (battlePoint > 0) {
-			//choose challenger
-			while (!canBattle) {
-				i--;
-				console.log(i);
-				if (i < 0) {
-					canBattle = true;
-				} else {
-					challenger = xaky_table.rows[i];
-					challengerFight = challenger.cells[3].innerText[0] - 0;
-					if (!challengerFight.isNaN && challengerFight < 3) {
-						canBattle = true;
-						//xaky_table.rows[i].click();
-						xkevent(xaky_table.rows[i], 'click');
-						setTimeout(function() {
-							//document.querySelector('.blue_button_L').click();
-							xkevent(document.querySelector('.blue_button_L'), 'click');
-						}, myRandom('challengeTower', 500, 1000));
-					}
-				}
-			}
-		}
-	} else {
-		var purpleButton = document.querySelector('.purple_button_L');
-		if (purpleButton.display !== 'none') {
-			xkevent(purpleButton, 'click');
-		}
-	}
-	
-	setTimeout(function() {
-		window.location = '/home.html';
-	}, myRandom('home', 6000, 12000));
-	
+		}, myRandom('home', 6000, 12000));
+		
 /*** LEAGUE BATTLE ***/
-} else if (window.location.href.indexOf('/battle.html?league_battle') !== -1) {
-	console.log('LEAGUE BATTLE');
-	
-	setTimeout(function() {
-		var xknode = document.querySelector('button[rel="launch"]');
-		xkevent(xknode, 'click');
-	}, myRandom('home', 1000, 3000));
-	
-	setTimeout(function() {
-		window.location = '/tower-of-fame.html';
-	}, myRandom('towerOfFame', 3000, 6000));
-	
+	} else if (window.location.href.indexOf('/battle.html?league_battle') !== -1) {
+		console.log('LEAGUE BATTLE');
+		
+		setTimeout(function() {
+			var xknode = document.querySelector('button[rel="launch"]');
+			xkevent(xknode, 'click');
+		}, myRandom('home', 1000, 3000));
+		
+		setTimeout(function() {
+			window.location = '/tower-of-fame.html';
+		}, myRandom('towerOfFame', 3000, 6000));
+		
 /*** NO EXTENSION ***/
-} else if (window.location.href === 'https://www.hentaiheroes.com/') {
-	console.log('GO TO HOME');
-	
-	setTimeout(function() {
-		window.location = '/home.html';
-	}, myRandom('home', 1000, 3000));
+	} else if (window.location.href === 'https://www.hentaiheroes.com/') {
+		console.log('GO TO HOME');
+		
+		setTimeout(function() {
+			window.location = '/home.html';
+		}, myRandom('home', 1000, 3000));
+			
+/*** INTRO ***/
+	} else if (window.location.href.indexOf('/intro') !== -1) {
+		console.log('intro bugg ?');
+		
+		setTimeout(function() {
+			window.location = '/home.html';
+		}, myRandom('home', 6000, 9000));
 	
 /*** IF NOTHING ***/
-} else {
-	setTimeout(function() {
-		window.location = '/home.html';
-	}, myRandom('home', 60000, 75000));
+	} else {
+		if (!xkmap['XKSWITCH']) {
+			setTimeout(function() {
+				window.location = '/home.html';
+			}, myRandom('home', 60000, 75000));
+		}
+	}
 }
 
 
